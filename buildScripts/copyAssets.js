88c1c4e5b0627eefbdbb142f40e53e193c039379
Dan Koenig <dan.koenig@helloworld.com>
2021-08-29 14:34:36 -0400

    setting up project

--- buildScripts/copyAssets.js
+++ buildScripts/copyAssets.js
@@ -1,5 +1,6 @@
 import ncp from 'ncp';
 ncp('src/assets','dist/assets',function(err){
+  console.log('copy assets')
   if(err){return console.log(err);}
   console.log('assets copeid');
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import './index.css'
-import {TweenLite} from 'gsap';
-import axios from 'axios';
-var nums = {a: 1, b: 2};
-TweenLite.to(nums, .5, {a: 50, onUpdate: function (o,p) {
-  console.log(o[p]);
-}, onUpdateParams: [nums,'a']});
-axios.get('bundle.js').then((response) => {
-  console.log(response);
-});
