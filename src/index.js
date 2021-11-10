4291ef2449d21fb7962b8ef0a62c8ca3f7817143
vicky lu <vicky.lu@IC-c02fq6m5md6r.local>
2021-11-09 20:36:31 -0500

    add util

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './index.css'
+import {baseURL, createDomElement} from './utilis.js';
+
+window.addEventListener('load', ()=>{
+    
+
+
+    // const bodyEl = document.querySelector('body');
+    // const imgEl = createDomElement('img',{src:`${baseURL}logo.png`});
+    // bodyEl.appendChild(imgEl);
+});
